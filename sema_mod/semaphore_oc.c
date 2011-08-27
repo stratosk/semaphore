3a6f9f3880cd6464877f0a882d8380632de6cff1
stratosk <stratosk@albert.(none)>
2011-08-27 21:06:28 +0300

    OC module

---
+++ sema_mod/semaphore_oc.c
@@ -0,0 +1,27 @@
+#include <linux/init.h>
+#include <linux/module.h>
+
+
+extern void s5pv210_change_high_1200(void);
+extern void s5pv210_change_high_1000(void);
+
+static int oc_init(void)
+{
+	printk(KERN_INFO "Overcloking enabled\n");
+	s5pv210_change_high_1200();
+	
+	return 0;
+}
+
+static void oc_exit(void)
+{
+	printk(KERN_INFO "Overclocking disabled\n");
+	s5pv210_change_high_1000();
+}
+
+module_init(oc_init);
+module_exit(oc_exit);
+
+MODULE_AUTHOR("stratosk");
+MODULE_DESCRIPTION("Semaphore overcloking");
+MODULE_LICENSE("GPL");
