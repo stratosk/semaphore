17a6480bd88e8014d822bf6374557f30a6e72bfb
stratosk <stratosk@albert.(none)>
2011-09-29 23:01:31 +0300

    Updated scripts for Script Manager application

--- sema_init_jvs/sbin/boot/install.sh
+++ sema_init_jvs/sbin/boot/install.sh
@@ -1,12 +1,26 @@
-if /sbin/ext/busybox [ ! -f /system/cfroot/release-80-JVR-s1- ]; 
+if /sbin/ext/busybox [ ! -f /system/cfroot/release-80-JVR-s2- ]; 
 then
 # Remount system RW
     /sbin/ext/busybox mount -o remount,rw /system
 
 # Remove startup and shutdown sounds, to make some space
-    toolbox rm /system/etc/PowerOn.snd
-    toolbox rm /system/etc/PowerOn.wav
-    toolbox rm /system/media/audio/ui/PowerOff.wav
+    if /sbin/ext/busybox [ -f /system/etc/PowerOn.snd ];
+    then
+    	toolbox cat /system/etc/PowerOn.snd > /data/local/PowerOn.snd
+	toolbox rm /system/etc/PowerOn.snd
+    fi;
+
+    if /sbin/ext/busybox [ -f /system/etc/PowerOn.wav ];
+    then
+	toolbox cat /system/etc/PowerOn.wav > /data/local/PowerOn.wav
+	toolbox rm /system/etc/PowerOn.wav
+    fi;
+
+    if /sbin/ext/busybox [ -f /system/media/audio/ui/PowerOff.wav ];
+    then
+    	toolbox cat /system/media/audio/ui/PowerOff.wav > /data/local/PowerOff.wav
+	toolbox rm /system/media/audio/ui/PowerOff.wav
+    fi;
 
 # ensure /system/xbin exists
     toolbox mkdir /system/xbin
@@ -51,13 +65,22 @@ then
     toolbox chown 0.0 /system/app/TweakManager.apk
     toolbox chmod 644 /system/app/TweakManager.apk
 
+# Semaphore Script Manager
+    toolbox rm /system/app/Semaphore.apk
+    toolbox rm /data/dalvik-cache/*semaphore.apk*
+    toolbox rm /data/app/hm.semaphore*.apk
+
+    toolbox cat /res/misc/Semaphore.apk > /system/app/Semaphore.apk
+    toolbox chown 0.0 /system/app/Semaphore.apk
+    toolbox chmod 644 /system/app/Semaphore.apk
+
 # Once be enough
     toolbox mkdir /system/cfroot
     toolbox chmod 755 /system/cfroot
     toolbox rm /data/cfroot/*
     toolbox rmdir /data/cfroot
     toolbox rm /system/cfroot/*
-    echo 1 > /system/cfroot/release-80-JVR-s1-
+    echo 1 > /system/cfroot/release-80-JVR-s2-
 
 # Remount system RO
     /sbin/ext/busybox mount -o remount,ro /system
