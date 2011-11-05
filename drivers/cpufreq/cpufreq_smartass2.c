f285e9189bc0e90ad4cb5dfcadac5ab8f9e39c89
stratosk <stratosk@albert.(none)>
2011-11-05 13:02:18 +0200

    Fix cpufreq_stats issue

--- drivers/cpufreq/cpufreq_smartass2.c
+++ drivers/cpufreq/cpufreq_smartass2.c
@@ -101,7 +101,7 @@ static unsigned long down_rate_us;
  * The frequency to set when waking up from sleep.
  * When sleep_ideal_freq=0 this will have no effect.
  */
-#define DEFAULT_SLEEP_WAKEUP_FREQ (1000*1000)
+#define DEFAULT_SLEEP_WAKEUP_FREQ (800*1000)
 static unsigned int sleep_wakeup_freq;
 
 /*
